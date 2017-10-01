c7fb9486fc0b64622a8fc55eb319765478ecb457
Iliar Turdushev <iliar.turdushev@gmail.com>
2017-10-01 07:11:40 +0600

    Добавил класс для вычисления ветра в размерных переменных

---
+++ LiquidDynamics/LiquidDynamics/Wind/IWind.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LiquidDynamics.Wind
+{
+   public class Wind
+   {
+      public double[,] Wx { get; private set; }
+      public double[,] Wy { get; private set; }
+   }
+
+   public class Tau
+   {
+      public double[,] TauX { get; private set; }
+      public double[,] TauY { get; private set; }
+   }
+
+   public class WindCalculator
+   {
+      private readonly Tau _tau;
+
+      public WindCalculator(Tau tau)
+      {
+         if (tau == null) throw new ArgumentNullException(nameof(tau));
+         _tau = tau;
+      }
+
+      public Wind Calculate()
+      {
+
+
+         return new Wind();
+      }
+   }
+}
